2044b7d64032e365100d997dd63f515bfd275591
Pako <kross2500@mail.ru>
2023-12-26 15:02:40 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Matematik matematik1 = new Matematik();
-        matematik1.SquareSurf(5);
-        matematik1.CircleSurf(5);
-        matematik1.TriangleSurf(3, 4, 5);
+        matematik1.SquareSurf(3);
+        matematik1.CircleSurf(4);
+        matematik1.TriangleSurf(10, 6, 8);
     }
 }
